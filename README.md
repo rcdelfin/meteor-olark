bbfa0e96018a7788f240fe2e672ef928fbd44c83
Raymund Delfin <raymund@ultro.co.uk>
2016-04-19 01:06:18 +0800

    Update support for Meteor 1.3

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Software that lifts customer support and sales.
 
 ### Installing
 ```
-mrt add olark
+meteor add olark
 ```
 
 If you don't have a settings.json file, you need to add one and load it according to the Meteor documentation. http://docs.meteor.com/#meteor_settings
--- package.js
+++ package.js
@@ -1,12 +1,22 @@
 Package.describe({
-  summary: "Port of Olark chat."
+  name: 'rcdelfin:olark',
+  version: '0.1.1',
+  summary: 'Port of Olark chat. Meteor 1.3.1 support',
+  git: 'https://github.com/rcdelfin/meteor-olark',
+  documentation: 'README.md'
 });
 
-Package.on_use(function (api) {
+Package.onUse(function(api) {
+  api.versionsFrom('1.3.1');
+  api.use('ecmascript');
+  api.addFiles('olark.js', 'client');
 
-  api.add_files(['olark.js'], 'client');
-  
-  if (typeof api.export !== 'undefined') {
-    api.export(['Olark'], 'client');
-  }
+  api.export('Olark', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest')
+  api.use('ecmascript')
+  api.use('rcdelfin:olark')
+  api.addFiles('olark-tests.js')
 });
